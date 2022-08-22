8bd1fb4cbf2227ae586e121ac8d6dee66bbdd531
fabriciomarote <fabrii.cai93@gmail.com>
2022-08-22 11:23:44 -0300

    Se modifica el componente Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Skills.css';
-import { FaGithub, FaJs, FaLinkedin, FaHtml5, FaCss3Alt, FaReact, FaGitAlt } from "react-icons/fa";
+import { FaGithub, FaJs, FaHtml5, FaCss3Alt, FaReact, FaGitAlt } from "react-icons/fa";
+import Themes from '../theme/Themes'
+import Footer from '../components/Footer'
+import { ThemeProvider } from 'styled-components';
+import { SkillsContainer } from '../theme/Changes';
 
 const Skills = () => {
+
+   const [theme, setTheme] = useState(localStorage.getItem('theme'));
   
     return (
         <>
-            <div className='container'>
+            <ThemeProvider theme={Themes[theme]}>
+            <SkillsContainer> 
+            <div className='skills-container'>
                <div className='box-icons'>
-                  <div className='icon'>
-                     <FaJs size={90}/>
-                     <p>Javascript</p>
-                  </div>
-                  <div className='icon'>
-                     <FaGithub size={90}/>
-                     <p>Github</p>
-                  </div>
                   <div className='icon'>
                      <FaHtml5 size={90}/>
                      <p>HTML</p>
@@ -25,19 +25,29 @@ const Skills = () => {
                      <p>CSS</p>
                   </div>
                   <div className='icon'>
+                     <FaJs size={90}/>
+                     <p>Javascript</p>
+                  </div>
+                  <div className='icon'>
                      <FaReact size={90}/>
                      <p>React</p>
                   </div>
                   <div className='icon'>
-                     <FaLinkedin size={90}/>
-                     <p>CSS</p>
+                     <FaGithub size={90}/>
+                     <p>Github</p>
                   </div>
+
                   <div className='icon'>
                      <FaGitAlt size={90}/>
                      <p>Git</p>
                   </div>
                </div> 
+               <div className='footer'>
+                    <Footer theme={theme} setTheme={setTheme}/> 
+               </div>   
             </div>
+            </SkillsContainer>
+            </ThemeProvider> 
         </>  
     );
   }
