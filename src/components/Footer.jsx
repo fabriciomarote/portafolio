992488e34bd38f1dd4f1b05b629788c4b6781c2c
fabriciomarote <fabrii.cai93@gmail.com>
2022-08-22 11:22:05 -0300

    Se modifica y se agrega el footer con links funcionando

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState }  from 'react'
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { ThemeProvider } from 'styled-components';
+import { FooterContainer } from '../theme/Changes';
+import Themes from '../theme/Themes'
 import '../styles/Footer.css';
 
-const Footer = () => {
+const Footer = (props) => {
+
+  const { theme, setTheme } = props;
+
   return (
-    <div className="main-footer">
-      <div className="container">
-        <hr className='hr'/>
-        <p className="col-sm">
-          &copy;{new Date().getFullYear()} {"Desarrollada por Fabricio Marote para la materia Construcción de Interfaces de Usuarios, UNQ."}
-        </p>
-      </div>
-    </div>
+    <>
+      <ThemeProvider theme={Themes[theme]}>
+        <FooterContainer> 
+          <div className="main-footer">
+            <div className="footer-container">
+                <div className="icons-footer">
+                  <a href="https://github.com/fabriciomarote" className='icon-f'><FaGithub size={25}/></a>
+                  <a href="https://www.linkedin.com/in/fabricio-marote/" className='icon-f'><FaLinkedin size={25}/></a>
+                </div>
+              <p className="col-sm">
+                {"Creado por Fabricio Marote |"} &copy;{new Date().getFullYear()} {"todos los derechos reservados."}
+              </p>
+            </div>
+          </div>
+        </FooterContainer>
+      </ThemeProvider> 
+    </>
   )
 }
 
